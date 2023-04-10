69f3e97415d6760013680a487c9e94fae788bc28
Turgut Memiş <kalabamba49@gmail.com>
2023-04-10 23:19:01 +0300

    Update setup.js

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -9,9 +9,13 @@ module.exports = {
             if(!interaction.member.permissions.has(PermissionFlagsBits.Administrator) || !interaction.user.id === 157971450437959680 ) return interaction.followUp({content: "You need to have the `ADMINISTRATOR` permission to use this command!"})
             else {
                 try{
-                    if(!interaction.guild.channels.cache.find(channel => channel.name === "Personal Rooms" && channel.type === ChannelType.GuildCategory)) {
-                        let cat = new Promise((resolve, reject)=> {resolve(interaction.guild.channels.create({name:"Personal Rooms", type: ChannelType.GuildCategory, position: interaction.guild.channels.size, reason: "Setup by " + interaction.user.tag}))})
-                        await cat
+                    if (!interaction.guild.channels.cache.find(channel => channel.name === "room-commands" && channel.type === ChannelType.GuildText) || !interaction.guild.channels.cache.find(channel => channel.name === "Join to Create" && channel.type === ChannelType.GuildVoice) || !interaction.guild.channels.cache.find(channel => channel.name === "Personal Rooms" && channel.type === ChannelType.GuildCategory)) {
+                        
+                        if(!interaction.guild.channels.cache.find(channel => channel.name === "Personal Rooms" && channel.type === ChannelType.GuildCategory)) {
+                            let cat = new Promise((resolve, reject)=> {resolve(interaction.guild.channels.create({name:"Personal Rooms", type: ChannelType.GuildCategory, position: interaction.guild.channels.size, reason: "Setup by " + interaction.user.tag}))})
+                            await cat
+                        }
+
                         if(!interaction.guild.channels.cache.find(channel => channel.name === "Join to Create" && channel.type === ChannelType.GuildVoice)) {
                             interaction.guild.channels.create({
                                 name:"Join to Create", 
@@ -45,15 +49,14 @@ module.exports = {
                         .setDescription("Setup Complete!")
                         .setColor("Aqua")
                         return interaction.followUp({embeds: [embed]})
-                    }else {
-                            const embed = new EmbedBuilder()
-                            .setDescription("Setup Already Completed!")
-                            .setColor("Aqua")
-                            return interaction.followUp({embeds: [embed]})
-                    }  
+                    }else{
+                        const embed = new EmbedBuilder()
+                        .setDescription("Setup Already Completed!")
+                        .setColor("Aqua")
+                        return interaction.followUp({embeds: [embed]})
+                    }
                 }
                 catch(err) {console.log(err.message)}
-                
             }
         }
 };
